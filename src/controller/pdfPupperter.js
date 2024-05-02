4580262c09a4242b8aacae510a9e4d5ae2ebb429
Guhan M <139315570+Guhan-M@users.noreply.github.com>
2024-05-02 19:29:23 +0530

    Update pdfPupperter.js

--- src/controller/pdfPupperter.js
+++ src/controller/pdfPupperter.js
@@ -24,7 +24,7 @@ const generatepdf = async (req, res) => {
                 ],
                 protocolTimeout: 120000
             });
-
+              console.log(browser)
             const page = await browser.newPage();
             await page.goto(link, { waitUntil: "networkidle2" });
             await page.waitForTimeout(2000); // Adjust the delay as needed
@@ -32,7 +32,7 @@ const generatepdf = async (req, res) => {
                 const images = document.querySelectorAll('img');
                 return Array.from(images).every((img) => img.complete);
             });
-          
+          console.log(page)
             await page.setViewport({ width: 1080, height: 1024 });
 
             // Generate the PDF as a buffer
