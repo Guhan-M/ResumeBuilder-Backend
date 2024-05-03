67f00024970b8685eb8cf71a61103bb641b6124c
Guhan-M <guhan76guhan@gmail.com>
2024-05-03 09:39:33 +0530

    first commit

--- src/controller/pdfPupperter.js
+++ src/controller/pdfPupperter.js
@@ -7,7 +7,7 @@ dotenv.config()
 const generatepdf = async (req, res) => {
     console.log(req.body.id);
     try {
-        const link = `https://resumebuildergm.netlify.app/ResumetoPdf/${req.body.id}`;
+        const link = `http://localhost:5173/ResumetoPdf/${req.body.id}`;
       
         if (req.body) {
             const browser = await puppeteer.launch({
